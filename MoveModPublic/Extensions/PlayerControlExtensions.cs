5c116aee0ff80c8169e442f915f06f6b44c9f8cd
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2026-01-31 16:49:09 -0300

    use sendoption.none to prevent disconnections

--- MoveModPublic/Extensions/PlayerControlExtensions.cs
+++ MoveModPublic/Extensions/PlayerControlExtensions.cs
@@ -13,7 +13,7 @@ public static class PlayerControlExtensions
             target.SnapTo(position, (ushort)(target.lastSequenceId + 328));
         }
         ushort num = (ushort)(target.lastSequenceId + 8);
-        MessageWriter messageWriter = AmongUsClient.Instance.StartRpcImmediately(target.NetId, 21, SendOption.Reliable, -1);
+        MessageWriter messageWriter = AmongUsClient.Instance.StartRpcImmediately(target.NetId, 21, SendOption.None, -1);
         NetHelpers.WriteVector2(position, messageWriter);
         messageWriter.Write(num);
         AmongUsClient.Instance.FinishRpcImmediately(messageWriter);
